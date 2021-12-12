9e81c53224f3258ad5fc343e5cdbb900969aa90a
aderito95 <78804009+aderito95@users.noreply.github.com>
2021-12-12 18:25:23 +0100

    Stateless  widget complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex += 1;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What\'s you favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed:
+                  answerQuestion, //we are passing a pointer to the fuction that we want to execute when a button is pressed, so i dont need the () because otherwise the fuction is called instatly and the result is passed to onpressed
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //...
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
